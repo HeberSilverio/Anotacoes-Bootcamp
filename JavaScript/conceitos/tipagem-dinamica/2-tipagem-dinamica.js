8e7bf62918f0a5b1e03379906d9d91faf9b7e1e3
Héber Silvério <hebersilverio@gmail.com>
2020-11-08 19:24:36 -0300

    cria pastas e arquivos

---
+++ JavaScript/conceitos/tipagem-dinamica/2-tipagem-dinamica.js
@@ -0,0 +1,10 @@
+var minhaVariavel = 30; // cria uma variável número
+
+minhaVariavel = 'Texto'; //e altera para string
+
+console.log(minhaVariavel);
+
+// na tipagem dinâmica não é preciso explicitar o tipo da variável no momento da sua criação.
+//a partir do momento que você atribuir um valor a esta variável é que o compilador vai descobrir o tipo. Isso se chama inferência de tipo;
+
+//o java é de tipagem explicita e neste exemplo ele mostra o erro de incompatibilidade de tipo: o número não pode ser convertido para string.
---
+++ JavaScript/conceitos/tipagem-fraca/1-tipagem-fraca.js
@@ -0,0 +1,11 @@
+var meuNumero = 20;
+var meuTexto = 'Exemplo';
+
+console.log(meuNumero + meuTexto);
+
+//JavaScript é uma linguagem interpreta, ou seja, o código é executado de cima para baixo e o resultado desta execução do código é imediatamente retornado.
+
+//JavaScript é linguagem de tipagem fraca e dinâmica
+//tipagem fraca significa que não há uma verificação em todas as operações, ou seja, é possível utilizar o operador + com uma string e um número sem que ocorra um erro.
+
+//o python é de tipagem forte e mostrará um erro se você tentar executar esse exemplo.
