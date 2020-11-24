4baeb30316003450e5330da2920ca0a3a4210e54
Héber Silvério <hebersilverio@gmail.com>
2020-11-23 23:35:26 -0300

    destructuring

---
+++ JavaScript/conceitos/Destructuring/destructuring.js
@@ -0,0 +1,8 @@
+var arr = ['Apple', 'Banana', 'Orange'];
+
+var apple = arr[0];
+var banana = arr[1];
+var orange = arr[2];
+
+var [ apple2, banana2, orange2, [tomato2]] = ['Apple', 'Banana', 'Orange', ['tomato']];
+console.log(tomato2);
