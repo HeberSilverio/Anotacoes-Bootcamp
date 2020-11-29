2052c859b3788b321437adeac73b3b778673a5d7
Héber Silvério <hebersilverio@gmail.com>
2020-11-29 17:02:17 -0300

    APRENDENDO NODE

--- Node/index.js
+++ Node/index.js
@@ -1,1 +1,15 @@
-console.log('O node esta atualizado');
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Olá mundo pelo express!'))
+
+app.listen(port, () => console.log('Api rodando na porta 3000'))
